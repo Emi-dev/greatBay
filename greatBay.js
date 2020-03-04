fed2fbb56f825bf9d4f29c1c7b7c74160ee12d8f
Emi Clar <kotaki@gmail.com>
2020-03-03 20:25:20 -0500

    updated with asynch

--- greatBay.js
+++ greatBay.js
@@ -1,120 +1,125 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-const auctionQ = inquirer.prompt([
-  {
-    type: "list",
-    message: "Would you like to bid or post item?",
-    choices: ["Bid", "Post"],
-    name: "userAction"
-  },
-  {
-    type: "input",
-    message: "Enter your user ID",
-    name: "userID"
+async function init() {
+  const auctionQ = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Would you like to bid or post item?",
+      choices: ["Bid", "Post"],
+      name: "userAction"
+    },
+    {
+      type: "input",
+      message: "Enter your user ID",
+      name: "userID"
+    }
+  ]);
+
+  switch(auctionQ.userAction) {
+      case "Bid":
+          console.log("bid");
+          // const managerData = await inquirer.prompt(questions.manager);
+          // const manager = new Manager(employeeData.name, id, `${employeeData.name.replace(/\s/g, "")}@abc.com`, managerData.officeNumber);
+          // employeeCards += managerCard(manager);
+          break;
+      case "Post":
+          // const engineerData = await inquirer.prompt(questions.engineer);
+          // const engineer = new Engineer(employeeData.name, id, `${employeeData.name.replace(/\s/g, "")}@abc.com`, engineerData.github);
+          // employeeCards += engineerCard(engineer);
+          break;
   }
-]);
-
-switch(auctionQ.userAction) {
-    case "Bid":
-        const managerData = await inquirer.prompt(questions.manager);
-        const manager = new Manager(employeeData.name, id, `${employeeData.name.replace(/\s/g, "")}@abc.com`, managerData.officeNumber);
-        employeeCards += managerCard(manager);
-        break;
-    case "Post":
-        const engineerData = await inquirer.prompt(questions.engineer);
-        const engineer = new Engineer(employeeData.name, id, `${employeeData.name.replace(/\s/g, "")}@abc.com`, engineerData.github);
-        employeeCards += engineerCard(engineer);
-        break;
 }
 
-const connection = mysql.createConnection({
-  host: "localhost",
+init();
 
-  // Your port; if not 3306
-  port: 3306,
+// const connection = mysql.createConnection({
+//   host: "localhost",
 
-  // Your username
-  user: "root",
+//   // Your port; if not 3306
+//   port: 3306,
 
-  // Your password
-  password: "",
-  database: "greatBay_DB"
-});
+//   // Your username
+//   user: "root",
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
-});
+//   // Your password
+//   password: "",
+//   database: "greatBay_DB"
+// });
 
-function createProduct() {
-  console.log("Inserting a new product...\n");
-  const query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      flavor: "Rocky Road",
-      price: 3.0,
-      quantity: 50
-    },
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " product inserted!\n");
-      // Call updateProduct AFTER the INSERT completes
-      updateProduct();
-    }
-  );
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+//   createProduct();
+// });
 
-  // logs the actual query being run
-  console.log(query.sql);
-}
+// function createProduct() {
+//   console.log("Inserting a new product...\n");
+//   const query = connection.query(
+//     "INSERT INTO products SET ?",
+//     {
+//       flavor: "Rocky Road",
+//       price: 3.0,
+//       quantity: 50
+//     },
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " product inserted!\n");
+//       // Call updateProduct AFTER the INSERT completes
+//       updateProduct();
+//     }
+//   );
 
-function updateProduct() {
-  console.log("Updating all Rocky Road quantities...\n");
-  const query = connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
-      {
-        quantity: 100
-      },
-      {
-        flavor: "Rocky Road"
-      }
-    ],
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " products updated!\n");
-      // Call deleteProduct AFTER the UPDATE completes
-      deleteProduct();
-    }
-  );
+//   // logs the actual query being run
+//   console.log(query.sql);
+// }
 
-  // logs the actual query being run
-  console.log(query.sql);
-}
+// function updateProduct() {
+//   console.log("Updating all Rocky Road quantities...\n");
+//   const query = connection.query(
+//     "UPDATE products SET ? WHERE ?",
+//     [
+//       {
+//         quantity: 100
+//       },
+//       {
+//         flavor: "Rocky Road"
+//       }
+//     ],
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " products updated!\n");
+//       // Call deleteProduct AFTER the UPDATE completes
+//       deleteProduct();
+//     }
+//   );
 
-function deleteProduct() {
-  console.log("Deleting all strawberry icecream...\n");
-  connection.query(
-    "DELETE FROM products WHERE ?",
-    {
-      flavor: "strawberry"
-    },
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " products deleted!\n");
-      // Call readProducts AFTER the DELETE completes
-      readProducts();
-    }
-  );
-}
+//   // logs the actual query being run
+//   console.log(query.sql);
+// }
 
-function readProducts() {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    console.log(res);
-    connection.end();
-  });
-}
+// function deleteProduct() {
+//   console.log("Deleting all strawberry icecream...\n");
+//   connection.query(
+//     "DELETE FROM products WHERE ?",
+//     {
+//       flavor: "strawberry"
+//     },
+//     function(err, res) {
+//       if (err) throw err;
+//       console.log(res.affectedRows + " products deleted!\n");
+//       // Call readProducts AFTER the DELETE completes
+//       readProducts();
+//     }
+//   );
+// }
+
+// function readProducts() {
+//   console.log("Selecting all products...\n");
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     // Log all results of the SELECT statement
+//     console.log(res);
+//     connection.end();
+//   });
+// }
